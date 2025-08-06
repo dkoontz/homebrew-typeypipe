0e1c6d4412ddbcea1706c0bb6e137144150072ea
David Koontz <david@koontzfamily.org>
2025-08-06 02:57:36 -0700

    Update typeypipe to v0.3.0
    - Update macOS binary URL and SHA256
    - Update Linux binary URL and SHA256
    - Version: 0.3.0

--- Formula/typeypipe.rb
+++ Formula/typeypipe.rb
@@ -2,16 +2,16 @@ class Typeypipe < Formula
   desc "Shell wrapper that creates a mailbox system for external applications"
   homepage "https://github.com/dkoontz/TypeyPipe"
   license "MIT"
-  version "0.2.0"
+  version "0.3.0"
 
   on_macos do
-    url "https://github.com/dkoontz/TypeyPipe/releases/download/v0.2.0/typeypipe-v0.2.0-macos-x64.tar.gz"
-    sha256 "a881dd2881a3b890326bf2f6b7549d299e81b006d26c7f91deeae630b664e402"
+    url "https://github.com/dkoontz/TypeyPipe/releases/download/v0.3.0/typeypipe-v0.3.0-macos-x64.tar.gz"
+    sha256 "86d4113740d2772c0e0bf189e532d56aab3325d438ca4c2125543318b05073c8"
   end
 
   on_linux do
-    url "https://github.com/dkoontz/TypeyPipe/releases/download/v0.2.0/typeypipe-v0.2.0-linux-x64.tar.gz"
-    sha256 "b3c034e02961f28c2819f0b0cf800e474b141928acc9cacbf560e39027da1914"
+    url "https://github.com/dkoontz/TypeyPipe/releases/download/v0.3.0/typeypipe-v0.3.0-linux-x64.tar.gz"
+    sha256 "24e5ef5905d32a76175c62ca5115484acbd870011de14f516fda56438bb461c3"
   end
 
   def install
